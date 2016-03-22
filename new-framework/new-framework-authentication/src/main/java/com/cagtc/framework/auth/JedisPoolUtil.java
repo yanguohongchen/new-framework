b40e0bf61e144bfa75798e533a16f4070f7466d1
yanguohongchen <120228220@qq.com>
2016-03-22 17:19:43 +0800

    修改redis构造函数

--- new-framework/new-framework-authentication/src/main/java/com/cagtc/framework/auth/JedisPoolUtil.java
+++ new-framework/new-framework-authentication/src/main/java/com/cagtc/framework/auth/JedisPoolUtil.java
@@ -9,14 +9,12 @@ public class JedisPoolUtil
 {
 	private JedisPool jedisPool;
 
-	public JedisPoolUtil(String hostport, int maxIdle, int maxTotal)
+	public JedisPoolUtil(String host,int port, int maxIdle, int maxTotal)
 	{
 		GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
 		genericObjectPoolConfig.setMaxIdle(maxIdle);
 		genericObjectPoolConfig.setMaxTotal(maxTotal);
 		genericObjectPoolConfig.setTestOnBorrow(false);
-		String host = hostport.split(":")[0];
-		int port = Integer.parseInt(hostport.split(":")[1]);
 		jedisPool = new JedisPool(genericObjectPoolConfig, host, port);
 	}
 
